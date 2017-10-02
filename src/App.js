118d81eb98671ab5d258fcbf52b72cafebf4ab1b
Simon Vrachliotis <simon@simonswiss.com>
2017-10-02 15:00:29 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Headline = styled.h1`
+  color: tomato;
+  padding: 0.5rem;
+`;
+
+export default () => <Headline>Hello World!</Headline>;
