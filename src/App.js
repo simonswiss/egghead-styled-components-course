b5776c6220b9072c22eec9cfd0d4fb2cbc7ff517
Simon Vrachliotis <simon@simonswiss.com>
2017-10-15 09:05:18 +1100

    progress bar start

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
 import React from "react";
 import styled from "styled-components";
-import "./App.css";
 
-const Button = styled.button`
-  border: none;
-  margin: 6px;
-  padding: 0.85rem 1.5rem 0.75rem;
-  font-size: 0.9rem;
-  font-weight: 700;
-  border-bottom: solid 5px rgba(0, 0, 0, 0.1);
-  color: white;
-  cursor: pointer;
+import Button from "./components/Button";
 
-  border-radius: ${props => (props.pill ? "30px" : "4px")};
-  background: ${props => {
-    if (props.success) return "limegreen;";
-    if (props.warning) return "tomato;";
-    return "#a965cc;";
-  }}
-
-  &:hover {
-    border-bottom-width: 3px;
-    transform: translateY(2px);
-    margin-bottom: 8px;
-  }
-
-  &:active {
-    border-bottom-width: 1px;
-    transform: translateY(4px);
-    margin-bottom: 10px;
-  }
-
-  &:focus {
-    outline: none;
-  }
+const ProgressWrapper = styled.div`
+  position: relative;
+  height: 1.25rem;
+  width: 100%;
+  background: #f8f8f8;
+  margin: 1rem 0;
 `;
+
+const Progress = styled.div`
+  position: absolute;
+  background: #0099aa;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 40%;
+`;
+
+// width: ${props => props.status / props.target}
 
 export default () => (
   <div>
     <Button>Click me!</Button>
-    <Button pill>Pill button</Button>
-    <Button success>Success!</Button>
-    <Button warning pill>
-      Be careful!
-    </Button>
+    <ProgressWrapper>
+      <Progress />
+    </ProgressWrapper>
   </div>
 );
