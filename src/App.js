447391ea7531d4b416976301a4bad037af990702
Simon Vrachliotis <simon@simonswiss.com>
2017-10-15 14:33:37 +1100

    styled(OtherComponent)

--- src/App.js
+++ src/App.js
@@ -1,67 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import Button from "./components/Button";
-import InfoList from "./components/InfoList";
-import InfoItem from "./components/InfoItem";
-
-const ProgressWrapper = styled.div`
+const Quote = styled.blockquote`
   position: relative;
-  height: 1.25rem;
-  width: 100%;
-  background: #f2f2f2;
-  border-radius: 10px;
-  overflow: hidden;
+  background: #f7f7f7;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  padding-left: 4rem;
+  padding-right: 2rem;
+  margin: 0;
+  border-left: solid 5px rgba(0, 0, 0, 0.4);
+  font-family: Georgia, serif;
+  font-style: italic;
+  line-height: 1.5;
+  margin-bottom: 1rem;
+
+  &:before {
+    content: "“";
+    font-size: 4rem;
+    position: absolute;
+    left: 1.25rem;
+    top: 0;
+  }
 `;
 
-const Progress = styled.div`
-  position: absolute;
-  background: ${props =>
-    props.progress === props.target ? "limegreen" : "#a965cc"};
-  width: ${props => `${props.progress / props.target * 100}%`};
-  top: 0;
-  left: 0;
-  height: 100%;
-  transition: all 0.2s ease-in-out;
+const CalloutQuote = styled(Quote)`
+  background: limegreen;
+  color: white;
 `;
 
-// width: ${props => props.status / props.target}
-
-export default class extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      progress: 0,
-      target: 10
-    };
-    this.updateProgress = this.updateProgress.bind(this);
-  }
-
-  updateProgress(value) {
-    let { progress } = this.state;
-    progress = progress + value;
-
-    // only update state if progress is within range
-    if (progress >= 0 && progress <= this.state.target) {
-      this.setState({
-        progress: progress
-      });
-    }
-  }
-  render() {
-    const { progress, target } = this.state;
-    return (
-      <div>
-        <Button onClick={() => this.updateProgress(-1)}>-1</Button>
-        <Button onClick={() => this.updateProgress(1)}>+1</Button>
-        <ProgressWrapper>
-          <Progress progress={progress} target={target} />
-        </ProgressWrapper>
-        <InfoList>
-          <InfoItem label="Points" value={progress} />
-          <InfoItem label="Target" value={target} />
-        </InfoList>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <Quote>
+      I am super happy this has finally happened. I have been waiting for it for
+      so long!
+    </Quote>
+    <CalloutQuote>
+      I am super happy this has finally happened. I have been waiting for it for
+      so long!
+    </CalloutQuote>
+  </div>
+);
