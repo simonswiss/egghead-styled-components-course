524f2fc5b0f7b93348b7afaf26148ab3a2b68a3e
Simon Vrachliotis <simon@simonswiss.com>
2017-10-15 15:10:34 +1100

    textareas with small or big vertical rows presets via props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
-const Quote = styled.blockquote`
-  position: relative;
-  background: #f7f7f7;
-  padding-top: 1.5rem;
-  padding-bottom: 1.5rem;
-  padding-left: 4rem;
-  padding-right: 2rem;
-  margin: 0;
-  border-left: solid 5px rgba(0, 0, 0, 0.4);
-  font-family: Georgia, serif;
-  font-style: italic;
-  line-height: 1.5;
-  margin-bottom: 1rem;
-
-  &:before {
-    content: "“";
-    font-size: 4rem;
-    position: absolute;
-    left: 1.25rem;
-    top: 0;
+const TextArea = styled.textarea.attrs({
+  rows: props => {
+    if (props.small) return 3;
+    if (props.big) return 10;
   }
-`;
-
-const CalloutQuote = styled(Quote)`
-  background: limegreen;
-  color: white;
+})`
+  border: solid 2px #e6e6e6;
+  border-radius: 4px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  font-size: 1rem;
+  resize: vertical;
 `;
 
 export default () => (
   <div>
-    <Quote>
-      I am super happy this has finally happened. I have been waiting for it for
-      so long!
-    </Quote>
-    <CalloutQuote>
-      I am super happy this has finally happened. I have been waiting for it for
-      so long!
-    </CalloutQuote>
+    <TextArea small />
+    <TextArea big />
   </div>
 );
